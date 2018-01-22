55012fe1998596306fc8e1579e01bfd390aeb8c2
lukeyi147 <32581186+lukeyi147@users.noreply.github.com>
2018-01-22 13:09:46 -0500

    prepare to add lfu test case

---
+++ src/testing/LFUCacheTest.java
@@ -0,0 +1,10 @@
+package testing;
+
+import junit.framework.TestCase;
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Test;
+import server.cache.KVLFUCache;
+
+public class LFUCacheTest extends TestCase{
+	//TODO going to add test cases related to LFU cache
+}
