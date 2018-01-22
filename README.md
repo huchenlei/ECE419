c8eb449eed4820401b8b744010aa390c1e9448ec
lukeyi147 <32581186+lukeyi147@users.noreply.github.com>
2018-01-21 21:50:56 -0500

    implementing LFU cache strategy

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ PRA 0103
 Mon 12 ~ 3 PM
 - Chenlei Hu （1002030651）
 - Rongzhen Cui (1000221823)
-- Shiqi Yi (TODO)
+- Shiqi Yi (1000297341)
 
---
+++ src/server/cache/KVLFUCache.java
@@ -0,0 +1,55 @@
+package server.cache;
+
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class KVLFUCache extends AbstractKVCache {
+	
+	private HashMap<String,Integer> LFUMap;
+	
+	public KVLFUCache(int cacheSize) {
+		super(cacheSize);
+		this.cacheMap = new HashMap<String, String>();
+		this.LFUMap = new HashMap<String,Integer>(); 
+    }
+	
+	//apply this function before get and put for LFU strategy
+	public void LFUReplace(String key) {
+		int counter;
+		if (this.getCacheSize() == 0) {
+			return;
+		}
+		else if (this.cacheMap.containsKey(key)) {
+			//increment the counter of the key
+			counter = LFUMap.get(key);
+			counter += 1;
+			LFUMap.put(key, counter);
+			return;
+		}
+		else if (this.cacheMap.size() >= this.getCacheSize()) {
+		//remove the lfu entry
+			String keyToRemove;
+			Iterator itr = LFUMap.entrySet().iterator(); 
+			Map.Entry entry;
+			entry = (Map.Entry)itr.next();
+			counter = (int)entry.getValue();
+			keyToRemove = (String)entry.getKey();
+			while (itr.hasNext()) {
+				entry = (Map.Entry)itr.next();
+				if ((int)entry.getValue() < counter) {
+					counter = (int)entry.getValue();
+					keyToRemove = (String)entry.getKey();
+				}	
+			}
+			this.cacheMap.remove(keyToRemove);
+			LFUMap.remove(keyToRemove);
+		}
+		//record the counter of the new entry
+		//cache map entry is not inserted in this function
+		counter = 1;
+		LFUMap.put(key, counter);
+		return;
+	}
+}
