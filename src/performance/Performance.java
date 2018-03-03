7d0e0692b99b5b144d50237cb68dec847a8b863b
lukeyi147 <32581186+lukeyi147@users.noreply.github.com>
2018-03-03 00:09:08 -0500

    update performance class

--- src/performance/Performance.java
+++ src/performance/Performance.java
@@ -1,105 +1,161 @@
 package performance;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import app_kvServer.KVServer;
+import client.KVStore;
+import common.messages.KVMessage;
+import ecs.ECS;
+import ecs.IECSNode;
 import logger.LogSetup;
 
 public class Performance {
 	
 	private static Logger logger = Logger.getRootLogger();
-	private long getLatency;
-	private long putLatency;
-	private int number_of_get;
-	private int number_of_put;
-	private KVServer server;
+	private long totalGetLatency;
+	private long totalPutLatency;
+	private int numReqests; 
+	private Integer numer_of_servers;
+	private Integer numer_of_clients;
 	
-	public Performance (int port, int cacheSize, String strategy, String fileName) {
-		this.getLatency=0;
-		this.putLatency=0;
-		this.number_of_get=0;
-		this.number_of_put=0;
-		server = new KVServer(port, cacheSize, strategy, fileName);
-		server.clearStorage();
-		new Thread(server).start();
+	private ECS ecs = null;
+    private String CACHE_STRATEGY;
+    private Integer CACHE_SIZE;
+    private List<KVServer> servers = new ArrayList<>();
+    private List<KVStore> clients = new ArrayList<>();
+    private List<ClientSession> sessions = new ArrayList<>();
+    private List<KVMessage> msgs = DataParser.parseDataFrom("allen-p");
+	
+	public Performance (int cacheSize, String strategy, Integer numNodes, 
+			Integer numClients, int numReq) throws Exception {
+		this.totalGetLatency=0;
+		this.totalPutLatency=0;
+		this.numReqests = numReq;
+		this.numer_of_servers = numNodes;
+		this.numer_of_clients = numClients;
+		
+	    this.CACHE_SIZE = cacheSize;
+	    this.CACHE_STRATEGY = strategy;
+	    //initialize and start the required number of servers
+		ecs = new ECS("./ecs.config");
+		assertNotNull(ecs);
+		this.addNodes(numNodes);
+		boolean ret = ecs.start();
+        assertTrue(ret);
+        Thread.sleep(1000);
+        //initialize the required number of clients
+		int i, j;
+		Random rand = new Random();
+		for (i=0; i<numClients; i++) {
+			//get a random server first to connect
+			j = rand.nextInt(numNodes);
+			KVStore store = new KVStore(servers.get(j).getHostname(), servers.get(j).getPort());
+            store.connect();
+            clients.add(store);
+		}
 	}
 	
-	public long getLatency() {
-		return this.getLatency;
-	}
-	
-	public long putLatency() {
-		return this.putLatency;
-	}
+    public void addNodes(Integer count) throws Exception {
+        Collection<IECSNode> nodes = ecs.setupNodes(count, this.CACHE_STRATEGY, this.CACHE_SIZE);
+        assertNotNull(nodes);
+        assertEquals(count, new Integer(nodes.size()));
+        // Start the servers internally
+        for (IECSNode node : nodes) {
+            KVServer server = new KVServer(node.getNodePort(), node.getNodeName(),
+                    ECS.ZK_HOST, Integer.parseInt(ECS.ZK_PORT));
+            servers.add(server);
+            server.clearStorage();
+            new Thread(server).start();
+        }
+        boolean ret = ecs.awaitNodes(count, ECS.ZK_TIMEOUT);
+        assertTrue(ret);
+    }
+    
+    public void startTest() throws Exception {
+    		int num = this.numReqests / this.clients.size(); 
+    		for (KVStore client : clients) {
+    			ClientSession session = new ClientSession(client, this.msgs, num);
+    			this.sessions.add(session);
+    		}
+    		for (ClientSession session : sessions) {
+    			session.start();
+    		}
+    		//Let all the sessions finish running
+    		Thread.sleep(20000);
+    		while (true) {
+    			if (this.sessions.isEmpty()) {
+    				break;
+    			}
+    			for (Iterator<ClientSession> iterator = sessions.iterator(); iterator.hasNext(); ) {
+    				ClientSession session = iterator.next();
+    				if (session.finishFlag()) {
+    					this.totalGetLatency += session.getLantency();
+    					this.totalPutLatency += session.putLantency();
+    					iterator.remove();
+    				}
+    			}
+    		}
+    }
 	
 	public float totalLatency() {
-		long totalLat = this.getLatency+this.putLatency;
+		long totalLat = this.totalGetLatency+this.totalPutLatency;
 		logger.info("Total Latency in mili seconds is:" + totalLat/1000000);
 		return totalLat/1000000;
 	}
 	
 	public float averageLatency() {
-		float totalAvgLat = (this.getLatency+this.putLatency)/(number_of_get+number_of_put);
+		float totalAvgLat = (this.totalGetLatency+this.totalPutLatency)/(this.numReqests);
 		logger.info("Total average latency in mili seconds is:" + totalAvgLat/1000000);
 		return totalAvgLat/1000000;
 	}
 	
 	public float averagePutLatency() {
-		float averagePutLat = this.putLatency / this.number_of_put;
+		float averagePutLat = this.totalPutLatency / (this.numReqests/2);
 		logger.info("Average put latency in mili seconds is:" + averagePutLat/1000000 );
 		return averagePutLat/1000000;
 	}
 	
 	public float averageGetLatency() {
-		float averageGetLat = this.getLatency / this.number_of_get;
+		float averageGetLat = this.totalGetLatency / (this.numReqests/2);
 		logger.info("Average get latency in mili seconds is:" + averageGetLat/1000000 );
 		return averageGetLat/1000000;
 	}
 	
-	public void recordLatency(int numRequest, int putPortion, int getPortion) {
-		Random rand = new Random();
-		try {
-			this.number_of_get = (numRequest/(putPortion+getPortion))*getPortion;
-			this.number_of_put = (numRequest/(putPortion+getPortion))*putPortion;
-			int total = number_of_get+number_of_put;
-			long start = System.nanoTime();
-			for(int i=0; i<number_of_put; i++) {
-				server.putKV(String.valueOf((rand.nextInt(total/10) + 1)), "testingLatency");
-			}
-			long end = System.nanoTime();
-			this.putLatency = end-start;
-			start = System.nanoTime();
-			for(int i=0; i<number_of_get; i++) {
-				server.getKV(String.valueOf((rand.nextInt(total/10) + 1)));
-			}
-			end = System.nanoTime();
-			this.getLatency = end-start;	
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	public void Shutdown() throws Exception {
+        boolean ret = this.ecs.shutdown();
+        assertTrue(ret);
+    }
 	
 	public static void main(String[] args)  {
 		//define cache size here
-		int cacheSize=200;
-		//set it to be a multiple of 100
-		//key will be randomly select from 1 - numRequest/10
-		int numRequest=6000;
-		int port = 20000;
+		int cacheSize=50;
+		//ensure numRequest / numClients is a integer
+		//ensure numRequest is an even number
+		int numRequest=100;
 		String cacheStrategy = "FIFO";
-		int putPortion = 3;
-		int getPortion = 7;
+		int numClients = 5;
+		int numServers = 5;
+
 		try {
 			 new LogSetup("logs/testing/performance.log", Level.INFO);
-			 Performance performance = new Performance(port, cacheSize, cacheStrategy, "PerformanceDB");
-			 performance.recordLatency(numRequest, putPortion, getPortion);
+			 Performance performance = new Performance(cacheSize, cacheStrategy, 
+					 numServers, numClients, numRequest);
+			 performance.startTest();
 			 performance.totalLatency();
 			 performance.averageLatency();
 			 performance.averageGetLatency();
-			 performance.averagePutLatency();	 
+			 performance.averagePutLatency();
+			 performance.Shutdown();
 		}
 		catch (Exception e) {
             e.printStackTrace();
@@ -107,4 +163,63 @@ public class Performance {
 		
 	}
 
+}
+
+
+class ClientSession extends Thread {
+	private List<KVMessage> messages;
+	private KVStore client;
+	private int numReq;
+	private boolean finishRequests;
+	private long getLatency;
+	private long putLatency;
+	private int size;
+	
+	public ClientSession(KVStore store, List<KVMessage> msgs, int num) {
+		this.messages = msgs;
+		this.client = store;
+		this.numReq = num;
+		this.getLatency = 0;
+		this.putLatency = 0;
+		this.size = this.messages.size();
+		this.finishRequests = false;
+	}
+	
+	public long getLantency() {
+		return this.getLatency;
+	}
+	
+	public long putLantency() {
+		return this.putLatency;
+	}
+	
+	public boolean finishFlag() {
+		return this.finishRequests;
+	}
+	
+	@Override
+	public void run(){  
+		Random rand = new Random();
+		try {
+			int index;
+			long start = System.nanoTime();
+			for(int i=0; i< numReq/2; i++) {
+				index = rand.nextInt(this.size);
+				client.put(messages.get(index).getKey(), messages.get(index).getValue());
+			}
+			long end = System.nanoTime();
+			this.putLatency = end-start;
+			start = System.nanoTime();
+			for(int i=0; i< numReq/2; i++) {
+				index = rand.nextInt(this.size);
+				client.get(messages.get(index).getKey());
+			}
+			end = System.nanoTime();
+			this.getLatency = end-start;	
+			this.finishRequests = true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	} 
 }
